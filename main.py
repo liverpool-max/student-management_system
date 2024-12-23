d5f2d6cb906ce3632081f78c1c60e30b9f62ef1f
liverpool-max <thmzliorxan40@gmail.com>
2024-12-23 17:33:26 -0500

    end of the program

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from fastapi import FastAPI
-from login import login_router
-from users import user_router
+from fastapi import FastAPI, APIRouter
+from login import authentication_router
+from user import user_router
 from students import student_router
 from course import course_router
+from grade import grade_router
 
-app = FastAPI(title="Student management system",description="This app for course registration and lecturer, student")
+app = FastAPI()
+
+app.include_router(authentication_router)
+app.include_router(user_router)
+app.include_router(student_router)
+app.include_router(course_router)
+app.include_router(grade_router)
+
 
 @app.get("/")
-def heaalth_check():
-    return {'Message':"Health check is succesfull"}
-
-app.include_router(course_router)
-
-app.include_router(student_router)
-
-app.include_router(user_router)
-
-app.include_router(login_router)
+def helth_check():
+    return {"Message": "Hello World"}
--- students.py
+++ students.py
@@ -1,30 +1,45 @@
-from fastapi import APIRouter,Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
 from jwt import get_current_user
-from student_schema import *
-from student_service import *
+from sqlalchemy.orm import Session
 from db import get_db
+from schema import *
+from student_service import *
 
-student_router = APIRouter(tags=["students"],prefix="/students")
-
-
-@student_router.get("/")
-def get_all_students(db:Session = Depends(get_db),current_user = Depends(get_current_user)):
-    msg = get_all_students_in_db(db=db,current_user=current_user)
-    return msg
-
-@student_router.get("/{id}")
-def get_student_by_id(id:int,db:Session = Depends(get_db),current_user = Depends(get_current_user)):
-    msg = get_student_by_id_in_base(id=id,db=db,current_user=current_user)
-    return msg
+student_router = APIRouter(tags=["Student"])
 
 
-@student_router.post("/")
-def create_student(item:Studentcreateshcema,db:Session = Depends(get_db),current_user = Depends(get_current_user)):
-    msg = create_student_in_base(db=db,data=item,current_user=current_user)
-    return msg
+@student_router.get("/student")
+def get_all_student(
+    current_user=Depends(get_current_user), db: Session = Depends(get_db)
+):
+    message = get_all_student_from_db(current_user=current_user, db=db)
+    return message
 
-@student_router.delete("/")
-def delete_student(item:Studentdeletescheme,db:Session = Depends(get_db),current_user = Depends(get_current_user)):
-    msg = delete_student_by_id(db=db,data=item,current_user=current_user)
-    return msg
+
+@student_router.post("/student")
+def create_new_student(
+    *,
+    item: CreateNewStudent,
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    message = create_new_student_in_db(data=item, current_user=current_user, db=db)
+    return message
+
+
+@student_router.delete("/student/{id}")
+def delete_student(
+    id, 
+    current_user=Depends(get_current_user), 
+    db: Session = Depends(get_db)
+):
+    message = delete_student_from_db(id=id, current_user=current_user, db=db)
+    return message
+
+
+@student_router.get("/student/{id}")
+def get_all_student_data(
+    id: int, current_user=Depends(get_current_user), db: Session = Depends(get_db)
+):
+    message = get_all_student_data_from_db(id=id, current_user=current_user, db=db)
+    return message
